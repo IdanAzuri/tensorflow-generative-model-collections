21af883189c269448f75d75716342aee0feac37e
IdanAzuri <idan.a@taboola.com>
2018-05-07 12:18:30 +0300

    changing mu

--- Sampler.py
+++ Sampler.py
@@ -14,7 +14,7 @@ class Sampler(object):
 
 
 class MultivariateGaussianSampler(Sampler):
-	def get_sample(self, batch_size, embedding_dim, n_distributions,mu=0.1,sigma=0.15):
+	def get_sample(self, batch_size, embedding_dim, n_distributions,mu=1,sigma=0.15):
 		current_dist_states_indices = np.random.randint(0, n_distributions - 1, batch_size)
 		mean_vec = np.linspace(-mu,mu,n_distributions)
 		cov_mat = np.eye(n_distributions) * sigma  # np.random.randint(1, 5, n_distributions)  # this is diagonal beacuse we want iid
