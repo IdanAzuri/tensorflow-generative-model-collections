69334f7424df81c1bf2d05e2d4abe61d8e8fec7a
idanazuri <idan.azuri@gmail.com>
2018-06-04 15:16:05 +0300

    tf.nn.softmax_cross_entropy_with_logits_v2

--- classifier.py
+++ classifier.py
@@ -25,7 +25,7 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 import matplotlib
-# matplotlib.use('Agg')
+matplotlib.use('Agg')
 import argparse
 import pickle
 
