e840bb063c1acc8f1e28386665ad4df2fc169622
idanazuri <idan.azuri@gmail.com>
2018-06-25 12:53:54 +0300

    minor

--- classifier.py
+++ classifier.py
@@ -239,7 +239,7 @@ class CNNClassifier():
 	
 	def train(self, confidence_in_train=False, confidence_thresh=0.9):
 		start_batch_id = 0  # int(1000 / self.batch_size)
-		self.num_batches = len(self.data_X) // self.batch_size
+		self.num_batches = min(len(self.data_X) // self.batch_size,1500)
 		for epoch in range(self.num_epochs):
 			for i in range(start_batch_id, self.num_batches):
 				batch_images = self.data_X[i * self.batch_size:(i + 1) * self.batch_size].reshape(-1, self.IMAGE_WIDTH * self.IMAGE_HEIGHT)
