40a77bbbc6775d62388a54b977f430ed573f1a35
idanazuri <idan.azuri@gmail.com>
2018-06-25 09:55:50 +0300

    test size

--- MultiModalInfoGAN.py
+++ MultiModalInfoGAN.py
@@ -43,7 +43,7 @@ class MultiModalInfoGAN(object):
 	
 	def __init__(self, sess, epoch, batch_size, z_dim, dataset_name, checkpoint_dir, result_dir, log_dir, sampler, len_continuous_code=2, is_wgan_gp=False,
 	             dataset_creation_order=['czcc', 'czrc', 'rzcc', 'rzrc'], SUPERVISED=True, dir_results="classifier_results"):
-		self.test_size = 2000
+		self.test_size = 5000
 		self.wgan_gp = is_wgan_gp
 		self.loss_list = []
 		self.confidence_list = []
