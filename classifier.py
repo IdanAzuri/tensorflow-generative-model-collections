da37fac6127f43dc3774372b04f32ff359b55fb8
idanazuri <idan.azuri@gmail.com>
2018-06-27 12:13:59 +0300

    fixing confidence

--- classifier.py
+++ classifier.py
@@ -262,7 +262,7 @@ class CNNClassifier():
 					print('accuracy:{}, mean_confidence:{}, loss:{}'.format(accuracy, np.mean(confidence), loss))
 					self.accuracy_list.append(accuracy)
 				else:
-					if not confidence_in_train:
+					if confidence_in_train:
 						self.train_step.run(session=self.sess, feed_dict={self.x: batch_images, self.y_: batch_labels, self.keep_prob: self.dropout_prob})
 					else:
 						accuracy, confidence, loss = self.test(batch_images, batch_labels, epoch * i)
