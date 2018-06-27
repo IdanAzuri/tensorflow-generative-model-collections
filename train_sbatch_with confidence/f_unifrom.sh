7e1481ad184255bf1862fcd316ba6d8fcf1395d4
idanazuri <idan.azuri@gmail.com>
2018-06-27 13:36:42 +0300

    sbatch

--- train_sbatch_with confidence/f_unifrom.sh
+++ train_sbatch_with confidence/f_unifrom.sh
@@ -13,20 +13,20 @@ dir=/cs/labs/daphna/idan.azuri/tensorflow-generative-model-collections
 cd $dir
 source /cs/labs/daphna/idan.azuri/venv_64/bin/activate
 
-python3 main.py --gan_type MultiModalInfoGAN --epoch 40 --dataset fashion-mnist --sampler uniform --batch_size 64 --dataset_order "czcc czrc rzcc rzrc" --result_dir results_with_confidence_26_6
+python3 main.py --gan_type MultiModalInfoGAN --epoch 40 --dataset fashion-mnist --sampler uniform --batch_size 64 --dataset_order "czcc czrc rzcc rzrc" --result_dir results_with_confidence_27_6
 #python3 classifier.py --dir_name /cs/labs/daphna/idan.azuri/tensorflow-generative-model-collections/ --fname fashion-mnist_UniformSample_mu_0_sigma_0.15_czcc_czrc_rzcc_rzrc --preprocess True --original fashion-mnist
 python3 classifier.py --dir_name /cs/labs/daphna/idan.azuri/tensorflow-generative-model-collections/ --fname fashion-mnist_UniformSample_mu_0_sigma_0.15_czcc_czrc_rzcc_rzrc --original fashion-mnist --use_confidence True
 
-python3 main.py --gan_type MultiModalInfoGAN --epoch 40 --dataset fashion-mnist --sampler uniform --batch_size 64 --dataset_order "czcc rzcc czrc rzrc" --result_dir results_with_confidence_26_6
+python3 main.py --gan_type MultiModalInfoGAN --epoch 40 --dataset fashion-mnist --sampler uniform --batch_size 64 --dataset_order "czcc rzcc czrc rzrc" --result_dir results_with_confidence_27_6
 #python3 classifier.py --dir_name /cs/labs/daphna/idan.azuri/tensorflow-generative-model-collections/ --fname fashion-mnist_UniformSample_mu_0_sigma_0.15_czcc_rzcc_czrc_rzrc --preprocess True --original fashion-mnist
 python3 classifier.py --dir_name /cs/labs/daphna/idan.azuri/tensorflow-generative-model-collections/ --fname fashion-mnist_UniformSample_mu_0_sigma_0.15_czcc_rzcc_czrc_rzrc --original fashion-mnist --use_confidence True
 
-python3 main.py --gan_type MultiModalInfoGAN --epoch 40 --dataset fashion-mnist --sampler uniform --batch_size 64 --dataset_order "rzcc rzrc czcc czrc" --result_dir results_with_confidence_26_6
+python3 main.py --gan_type MultiModalInfoGAN --epoch 40 --dataset fashion-mnist --sampler uniform --batch_size 64 --dataset_order "rzcc rzrc czcc czrc" --result_dir results_with_confidence_27_6
 #python3 classifier.py --dir_name /cs/labs/daphna/idan.azuri/tensorflow-generative-model-collections/ --fname fashion-mnist_UniformSample_mu_0_sigma_0.15_rzcc_rzrc_czcc_czrc --preprocess True --original fashion-mnist
 python3 classifier.py --dir_name /cs/labs/daphna/idan.azuri/tensorflow-generative-model-collections/ --fname fashion-mnist_UniformSample_mu_0_sigma_0.15_rzcc_rzrc_czcc_czrc --original fashion-mnist --use_confidence True
 
 
-python3 main.py --gan_type MultiModalInfoGAN --epoch 40 --dataset fashion-mnist --sampler uniform --batch_size 64 --dataset_order "czrc czcc rzcc rzrc" --result_dir results_with_confidence_26_6
+python3 main.py --gan_type MultiModalInfoGAN --epoch 40 --dataset fashion-mnist --sampler uniform --batch_size 64 --dataset_order "czrc czcc rzcc rzrc" --result_dir results_with_confidence_27_6
 #python3 classifier.py --dir_name /cs/labs/daphna/idan.azuri/tensorflow-generative-model-collections/ --fname fashion-mnist_UniformSample_mu_0_sigma_0.15_czrc_czcc_rzcc_rzrc --preprocess True --original fashion-mnist
 python3 classifier.py --dir_name /cs/labs/daphna/idan.azuri/tensorflow-generative-model-collections/ --fname fashion-mnist_UniformSample_mu_0_sigma_0.15_czrc_czcc_rzcc_rzrc --original fashion-mnist --use_confidence True
 
