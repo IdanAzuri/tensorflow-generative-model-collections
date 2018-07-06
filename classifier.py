2675d6623748b84446e0751923ad04035910afd5
idanazuri <idan.azuri@gmail.com>
2018-07-06 12:18:33 +0300

    sbatch VERY SMALL learning rate

--- classifier.py
+++ classifier.py
@@ -32,7 +32,7 @@ import warnings
 import matplotlib
 
 
-LEARNING_RATE = 1e-5
+LEARNING_RATE = 1e-6
 
 matplotlib.use('Agg')
 
@@ -253,7 +253,7 @@ class CNNClassifier():
 				# plt.title(batch_labels[0])
 				# plt.imshow(batch_images[0].reshape(28, 28))
 				# plt.show()
-				if i % 500 == 0:
+				if i % 300 == 0:
 					self.test_labels, self.test_images = shuffle(self.test_labels, self.test_images, random_state=0)
 					accuracy, confidence, loss = self.test(self.test_images[:1000].reshape(-1, 784), self.test_labels[:1000].reshape(-1, 10), epoch * i)
 					# summary, _ = self.sess.run([self.merged, self.train_step],
---
+++ train_sbatch_for_article/unifrom.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --mem=60g
+#SBATCH -c 30
+#SBATCH --gres=gpu:1
+#SBATCH --time=1-00
+#SBATCH --mail-user=idan.azuri@mail.huji.ac.il
+#SBATCH --mail-type=END,FAIL,TIME_LIMIT
+
+module load tensorflow/1.5.0
+
+dir=/cs/labs/daphna/idan.azuri/tensorflow-generative-model-collections
+
+cd $dir
+source /cs/labs/daphna/idan.azuri/venv_64/bin/activate
+
+python3 main.py --gan_type MultiModalInfoGAN --epoch 60 --dataset mnist --sampler uniform --batch_size 64 --dataset_order "czcc czrc rzcc rzrc"
+python3 classifier.py --dir_name /cs/labs/daphna/idan.azuri/tensorflow-generative-model-collections/ --fname mnist_UniformSample_mu_0_sigma_0.15_czcc_czrc_rzcc_rzrc --original mnist
+
