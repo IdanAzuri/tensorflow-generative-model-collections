fd7b9b804267eabbee75b2bdd4afa34d48fb0b0e
idanazuri <idan.azuri@gmail.com>
2018-07-08 00:50:36 +0300

    sbatch

--- train_sbatch_with confidence/unifrom.sh
+++ train_sbatch_with confidence/unifrom.sh
@@ -13,7 +13,6 @@ dir=/cs/labs/daphna/idan.azuri/tensorflow-generative-model-collections
 cd $dir
 source /cs/labs/daphna/idan.azuri/venv_64/bin/activate
 
-python3 classifier.py --dir_name /cs/labs/daphna/idan.azuri/tensorflow-generative-model-collections/ --fname mnist_UniformSample_mu_0_sigma_0.15_czcc_czrc_rzcc_rzrc --original mnist --use_confidence True
 python3 main.py --gan_type MultiModalInfoGAN --epoch 40 --dataset mnist --sampler uniform --batch_size 64 --dataset_order "czcc czrc rzcc rzrc" --result_dir results_with_confidence --original fashion-mnist
 python3 classifier.py --dir_name /cs/labs/daphna/idan.azuri/tensorflow-generative-model-collections/ --fname mnist_UniformSample_mu_0_sigma_0.15_czcc_czrc_rzcc_rzrc --original mnist --use_confidence True
 ##python3 classifier.py --dir_name /cs/labs/daphna/idan.azuri/tensorflow-generative-model-collections/ --fname mnist_UniformSample_mu_0_sigma_0.15_czcc_czrc_rzcc_rzrc --preprocess True --original mnist
