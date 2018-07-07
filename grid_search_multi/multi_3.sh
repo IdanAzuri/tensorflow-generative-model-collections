b73744bd9413cb3b9ad13d2b936f889ecf8b7206
idanazuri <idan.azuri@gmail.com>
2018-07-07 11:43:02 +0300

    sbatch

---
+++ grid_search_multi/multi_3.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#SBATCH --mem=60g
+#SBATCH -c 30
+#SBATCH --gres=gpu:1
+#SBATCH --time=0-20
+#SBATCH --mail-user=idan.azuri@mail.huji.ac.il
+#SBATCH --mail-type=END,FAIL,TIME_LIMIT
+
+module load tensorflow/1.5.0
+
+dir=/cs/labs/daphna/idan.azuri/tensorflow-generative-model-collections
+
+cd $dir
+source /cs/labs/daphna/idan.azuri/venv_64/bin/activate
+
+
+
+
+
+
+
+python3 main.py --gan_type MultiModalInfoGAN --epoch 60 --dataset mnist --sampler multi-gaussian --batch_size 64 --mu 0.1 --sigma 0.5
+python3 main.py --gan_type MultiModalInfoGAN --epoch 60 --dataset mnist --sampler multi-gaussian --batch_size 64 --mu 0.1 --sigma 1.0
+
+
+python3 classifier.py --dir_name /cs/labs/daphna/idan.azuri/tensorflow-generative-model-collections/ --fname mnist_MultivariateGaussianSampler_mu_0.1_sigma_0.5 --original mnist
+python3 classifier.py --dir_name /cs/labs/daphna/idan.azuri/tensorflow-generative-model-collections/ --fname mnist_MultivariateGaussianSampler_mu_0.1_sigma_1.0 --original mnist
