d14da9cd2aeba1050b960f3f88a462b93ceb80ed
idanazuri <idan.azuri@github.com>
2018-08-15 13:32:06 +0300

    minor

--- MultiModalInfoGAN.py
+++ MultiModalInfoGAN.py
@@ -313,7 +313,7 @@ class MultiModalInfoGAN(object):
 		start_time = time.time()
 		for epoch in range(start_epoch, self.epoch):
 			# get batch data
-			for idx in range(3):#(start_batch_id, self.num_batches):
+			for idx in range(start_batch_id, self.num_batches):
 				if self.dataset_name != "celebA":
 					batch_images = self.data_X[idx * self.batch_size:(idx + 1) * self.batch_size]
 				else:
