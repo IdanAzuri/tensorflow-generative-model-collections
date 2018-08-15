218607448d2b281347fc334e58949d1590dc9508
idanazuri <idan.azuri@gmail.com>
2018-08-15 11:34:29 +0300

    seed

--- classifier.py
+++ classifier.py
@@ -440,7 +440,7 @@ def main():
 		
 		# else:
 		c = CNNClassifier("custom", load_from_pkl=True, pkl_fname=fname, dir=dir, original_dataset_name=original_dataset_name)
-		c.train(confidence_in_train, confidence_thresh)
+		c.train(False, confidence_thresh)
 
 	else:
 		main_to_train_classifier()
