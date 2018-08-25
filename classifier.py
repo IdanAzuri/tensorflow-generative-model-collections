8ec2ed820e3cc64d4e743facd889b7106dd16be0
idanazuri <idan.azuri@gmail.com>
2018-08-25 13:25:34 +0300

    split train test

--- classifier.py
+++ classifier.py
@@ -476,7 +476,7 @@ def main_to_train_classifier():
 	confidence_thresh = args.confidence_thresh
 	data_X, data_y = load_mnist(original_dataset_name)
 	X_train, X_test, y_train, y_test = train_test_split(data_X, data_y, test_size=0.1, random_state=10)
-	c = CNNClassifier("custom", load_from_pkl=True, pkl_fname=fname, dir=dir, original_dataset_name=original_dataset_name, data_X=X_train, data_y=y_train, test_x=X_test,
+	c = CNNClassifier(original_dataset_name, original_dataset_name=original_dataset_name, pkl_fname=None, data_X=X_train, data_y=y_train, test_x=X_test,
 	                  test_y=y_test)
 	c.train()
 	c.test(X_test,y_test)
