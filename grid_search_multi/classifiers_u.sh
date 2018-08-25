f7de3b2e6d86c1543d21d6661b4515f18f2cf945
idanazuri <idan.azuri@gmail.com>
2018-08-25 22:03:37 +0300

    minor

--- grid_search_multi/classifiers_u.sh
+++ grid_search_multi/classifiers_u.sh
@@ -18,5 +18,5 @@ source /cs/labs/daphna/idan.azuri/venv_64/bin/activate
 
 
 
-python3 classifier.py --dir_name /cs/labs/daphna/idan.azuri/tensorflow-generative-model-collections/ --fname mnist_MultiModalUniformSample_mu_0_sigma_0.15_czcc_czrc_rzcc_rzrc_ndist_10 --original mnist
-python3 classifier.py --dir_name /cs/labs/daphna/idan.azuri/tensorflow-generative-model-collections/ --fname fashion-mnist_GaussianSample_mu_0.0_sigma_0.1_czcc_czrc_rzcc_rzrc_ndist_10 --original fashion-mnist
+python3 classifier.py --dir_name /cs/labs/daphna/idan.azuri/tensorflow-generative-model-collections/ --fname mnist_MultiModalUniformSample_mu_0_sigma_0.15_ndist_10 --original mnist
+python3 classifier.py --dir_name /cs/labs/daphna/idan.azuri/tensorflow-generative-model-collections/ --fname fashion-mnist_GaussianSample_mu_0.0_sigma_0.1_ndist_10 --original fashion-mnist
