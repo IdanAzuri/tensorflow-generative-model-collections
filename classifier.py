e9de278a55fa0136979e74d0038682ddc6becb7d
idanazuri <idan.azuri@gmail.com>
2018-08-26 16:38:56 +0300

    minor

--- classifier.py
+++ classifier.py
@@ -467,7 +467,7 @@ def main():
         print("Starting cross validation")
         cv = 4
         for i in range(cv):
-            print("Iteration {}/{}".format(i, 10))
+            print("Iteration {}/{}".format(i, cv))
             # X_train, X_test, y_train, y_test = train_test_split(data_X, data_y, test_size=0.001, random_state=10 + i)
             X_train_real, X_test_real, y_train_real, y_test_real = train_test_split(data_X_real, data_y_real,
                                                                                     test_size=0.2,
@@ -486,7 +486,6 @@ def main():
             c = CNNClassifier("custom", pkl_fname=fname, data_X=X_train, data_y=y_train, test_X=X_test_real,
                               test_y=y_test_real)
             accuracy_cross_validation.append(c.train(confidence_in_train=confidence_in_train))
-            print("Acuuracy of iteration {} : {}".format(i, accuracy_cross_validation[-1]))
         pickle.dump(accuracy_cross_validation, open("{}.pkl".format("accuracy_cv_{}_mixdataset".format(cv)), 'wb'))
 
         c.save_and_plot_results_cv("accuracy_cv_{}_mixdataset".format(cv), accuracy_cross_validation)
