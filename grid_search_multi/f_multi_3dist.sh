d9f6e935f4f9b07c9c83cc7559c65db0bb89454c
idanazuri <idan.azuri@gmail.com>
2018-08-26 10:07:26 +0300

    minor

--- grid_search_multi/f_multi_3dist.sh
+++ grid_search_multi/f_multi_3dist.sh
@@ -1,4 +1,22 @@
-#!/usr/bin/env bash
+#!/bin/bash
+#SBATCH --mem=70g
+#SBATCH -c 16
+#SBATCH --gres=gpu:1
+#SBATCH --time=1-20
+# SBATCH --mail-user=idan.azuri@mail.huji.ac.il
+#SBATCH --mail-type=END,FAIL,TIME_LIMIT
+
+module load tensorflow/1.5.0
+
+dir=/cs/labs/daphna/idan.azuri/tensorflow-generative-model-collections
+
+cd $dir
+source /cs/labs/daphna/idan.azuri/venv_64/bin/activate
+
+
+
+
+
 python3 main.py --gan_type MultiModalInfoGAN --epoch 40 --dataset fashion-mnist --sampler multi-gaussian --batch_size 64 --mu 0.1 --sigma 0.1 --ndist 3
 python3 main.py --gan_type MultiModalInfoGAN --epoch 40 --dataset fashion-mnist --sampler multi-gaussian --batch_size 64 --mu 0.1 --sigma 0.17 --ndist 3
 python3 main.py --gan_type MultiModalInfoGAN --epoch 40 --dataset fashion-mnist --sampler multi-gaussian --batch_size 64 --mu 0.1 --sigma 0.2 --ndist 3
