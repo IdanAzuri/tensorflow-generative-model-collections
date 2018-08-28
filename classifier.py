a52578d11da62129f34d52c891aed7657b03de80
idanazuri <idan.azuri@gmail.com>
2018-08-28 17:06:14 +0300

    no prior

--- classifier.py
+++ classifier.py
@@ -455,23 +455,15 @@ def main():
 	cv = 3
 	for i in range(cv):
 		print("Iteration {}/{}".format(i, cv))
-		# X_train, X_test, y_train, y_test = train_test_split(data_X, data_y, test_size=0.001, random_state=10 + i)
 		X_train_real, X_test_real, y_train_real, y_test_real = train_test_split(data_X_real, data_y_real, test_size=0.2, random_state=10 + i)
 		print("X_train_real={}, data_X={}, y_test_real={}, y_test={}".format(len(X_train_real), len(data_X), len(y_test_real), len(data_y)))
-		len_dataX = min(len(X_train_real), len(data_X))
-		data_X = data_X[:len_dataX]
-		data_y = data_y[:len_dataX]
-		X_train = np.append(data_X, X_train_real).reshape(-1, 784)
-		y_train = np.append(data_y, y_train_real.reshape(-1, 10)).reshape(-1, 10)
-		# X_test = np.append(X_test_real, X_test).reshape(-1, 784)
-		# y_test = np.append(y_test_real.reshape(-1, 10), y_test).reshape(-1, 10)
-		X_train, y_train = shuffle((X_train, y_train), random_state=10 + i)
+		data_X, data_y = shuffle((data_X, data_y), random_state=10 + i)
 		
-		c = CNNClassifier(original_dataset_name, pkl_fname=fname, data_X=X_train, data_y=y_train, test_X=X_test_real, test_y=y_test_real,seed=seed,save_model=False)
+		c = CNNClassifier(original_dataset_name, pkl_fname=fname, data_X=data_X, data_y=data_y, test_X=X_test_real, test_y=y_test_real,seed=seed,save_model=False)
 		accuracy_cross_validation.append(c.train(confidence_in_train=confidence_in_train))
 	pickle.dump(accuracy_cross_validation, open("{}.pkl".format("accuracy_cv_{}_improve_model".format(cv)), 'wb'))
 	
-	c.save_and_plot_results_cv("accuracy_cv_{}_improve_model".format(cv), accuracy_cross_validation)
+	c.save_and_plot_results_cv("accuracy_cv_{}_gan_only_no_prior".format(cv), accuracy_cross_validation)
 
 
 def main_to_train_classifier():
