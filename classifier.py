27dd2230d2c351e995d0ae9c7e46f11832072b0f
idanazuri <idan.azuri@gmail.com>
2018-08-28 11:49:36 +0300

    seed as arg

--- classifier.py
+++ classifier.py
@@ -490,7 +490,7 @@ def main_to_train_classifier():
 	confidence_thresh = args.confidence_thresh
 	data_X, data_y = load_mnist(original_dataset_name)
 	X_train, X_test, y_train, y_test = train_test_split(data_X, data_y, test_size=0.2, random_state=10)
-	c = CNNClassifier(original_dataset_name, pkl_fname=None, data_X=X_train, data_y=y_train, test_X=X_test, test_y=y_test, save_model=False)
+	c = CNNClassifier(original_dataset_name, pkl_fname=None, data_X=X_train, data_y=y_train, test_X=X_test, test_y=y_test, save_model=True)
 	c.train(confidence_in_train=confidence_in_train)
 	c.test(X_test, y_test)
 
