2b6567c4600e56c13c4f4d53802c921c78c7343c
idanazuri <idan.azuri@gmail.com>
2018-09-04 18:41:09 +0300

    sbatch

--- grid_search_multi/f_multi_unifrom.sh
+++ grid_search_multi/f_multi_unifrom.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#SBATCH --mem=4g
-#SBATCH -c 4
+#SBATCH --mem=16g
+#SBATCH -c 8
 #SBATCH --gres=gpu:1
 #SBATCH --time=0-20
 # SBATCH --mail-user=idan.azuri@mail.huji.ac.il
 #SBATCH --mail-type=END,FAIL,TIME_LIMITc.il
 #SBATCH --mail-type=END,FAIL,TIME_LIMIT
-#SBATCH --array=0-8%4
+#SBATCH --array=0-8%8
 SEEDS=(88 125 12 7 49 21 23 45 11)
 SEED=${SEEDS[SLURM_ARRAY_TASK_ID]}
 module load tensorflow/1.5.0
@@ -14,7 +14,7 @@ module load tensorflow/1.5.0
 dir=/cs/labs/daphna/idan.azuri/tensorflow-generative-model-collections
 
 cd $dir
-source /cs/labs/daphna/idan.azuri/venv/bin/activate
+source /cs/labs/daphna/idan.azuri/venv_64/bin/activate
 
 python3 main.py --gan_type MultiModalInfoGAN --epoch 40 --dataset mnist --sampler multi-uniform --batch_size 64 --seed $SEED
 python3 classifier.py --dir_name /cs/labs/daphna/idan.azuri/tensorflow-generative-model-collections/ --fname fashion-mnist_MultiModalUniformSample_mu_0_sigma_0.15_ndist_10 --original fashion-mnist --seed $SEED
