3dae453f7a39760cfce31ab63f6e92aef1c56794
idanazuri <idan.azuri@gmail.com>
2018-09-04 15:38:42 +0300

    general plot

--- plot_results.py
+++ plot_results.py
@@ -1,6 +1,9 @@
 import sys
+from collections import defaultdict
 
 import matplotlib
+
+
 matplotlib.use('Agg')
 
 import numpy as np
@@ -16,10 +19,12 @@ dir3 = 'classifier_results_seed_125/'
 START = 3
 start = START
 END = 50
-#regex
+# regex
 # (classifier_MM.*_sigma_\d.\d)(.*)(_ndist_\d+)(_accuracy)(.pkl)
-#$1$3$4_cv_3_gan_only_no_prior$5
+# $1$3$4_cv_3_gan_only_no_prior$5
 "classifier_MMinfoGAN_fashion-mnist_MultivariateGaussianSampler_mu_0.1_sigma_0.17_ndist_3_accuracy_cv_3_gan_only_no_prior.pkl"
+
+
 # 10 modalities
 def fashion_MM_plot_from_pkl():
 	# plt.Figure(figsize=(15, 15))
@@ -28,7 +33,8 @@ def fashion_MM_plot_from_pkl():
 	b = np.mean(
 		pickle.load(open(dir + "classifier_MMinfoGAN_fashion-mnist_MultivariateGaussianSampler_mu_0.7_sigma_0.3_ndist_5_accuracy_cv_3_gan_only_no_prior.pkl", "rb"))[START:END])
 	c = np.mean(pickle.load(open(dir + "classifier_MMinfoGAN_fashion-mnist_UniformSample_mu_0.0_sigma_0.1_ndist_10_accuracy_cv_3_gan_only_no_prior.pkl", "rb"))[START:END])
-	d = np.mean(pickle.load(open(dir + "classifier_MMinfoGAN_fashion-mnist_MultiModalUniformSample_mu_0.0_sigma_0.1_ndist_10_accuracy_cv_3_gan_only_no_prior.pkl", "rb"))[START:END])
+	d = np.mean(
+		pickle.load(open(dir + "classifier_MMinfoGAN_fashion-mnist_MultiModalUniformSample_mu_0.0_sigma_0.1_ndist_10_accuracy_cv_3_gan_only_no_prior.pkl", "rb"))[START:END])
 	
 	a2 = np.mean(pickle.load(open(dir2 + "classifier_MMinfoGAN_fashion-mnist_GaussianSample_mu_0.0_sigma_0.2_ndist_10_accuracy_cv_3_gan_only_no_prior.pkl", "rb"))[START:END])
 	b2 = np.mean(
@@ -395,7 +401,6 @@ def MM_mu_05_07_08_zoom_plot_from_pkl_5_modals():
 
 
 def MM_mu_01_zoom_plot_from_pkl_5modals():
-	
 	a = np.mean(
 		pickle.load(open(dir + "classifier_MMinfoGAN_fashion-mnist_MultivariateGaussianSampler_mu_0.1_sigma_0.1_ndist_5_accuracy_cv_3_gan_only_no_prior.pkl", "rb"))[START:END])
 	# b = np.mean(pickle.load(open(dir + "classifier_MMinfoGAN_fashion-mnist_MultivariateGaussianSampler_mu_0.1_sigma_0.1_ndist_5_accuracy_cv_3_gan_only_no_prior.pkl", "rb"))[START:END])
@@ -766,15 +771,66 @@ def MM_mu_1_zoom_plot_from_pkl3modals():
 	plt.close()
 
 
+def MMgeneral_plot_from_pkl(groupby=""):
+	import glob, os
+	param_list = []
+	files_list = defaultdict(list)
+	dirs = [d for d in glob.iglob("/Users/idan.a/repos/tensorflow-generative-model-collections/classifier_results_seed_*")]
+	
+	for dir in dirs:
+		for f in glob.iglob("{}/*{}*.pkl".format(dir,groupby)):
+			fname=f.split("/")[-1]
+			tmp=fname.split("_")
+			mu=tmp[5]
+			sigma=tmp[7]
+			ndist=tmp[9]
+			param_list.append("$\Sigma={},\mu={}$".format(sigma,mu))
+			files_list[fname].append(np.max(pickle.load(open(f, "rb"))))
+	
+	
+	means=[]
+	std_errs=[]
+	for key in files_list.keys():
+		current_experiment = files_list[key]
+		means.append(np.mean(current_experiment, axis=0))
+		std_errs.append(np.std(current_experiment, axis=0) / len(current_experiment))
+	
+
+
+
+	fig, ax = plt.subplots()
+	models = set(param_list)
+	title = 'MMinfoGAN_Fsion-Mnist_multi-modal Multi modal Gaussian'
+	
+	ax.set_title(title, fontsize=10)
+	x_pos = np.arange(len(models))
+	ax.bar(x_pos, means, yerr=std_errs, align='center', alpha=0.5, ecolor='black', capsize=10)
+	ax.set_ylabel('Accuracy')
+	ax.set_xticks(x_pos)
+	ax.set_xticklabels(models)
+	# ax.set_title('Prior')
+	ax.yaxis.grid(True)
+	
+	# Save the figure and show
+	plt.tight_layout()
+	
+	plt.ylabel("Accuracy Score")
+	plt.grid(True)
+	plt.show()
+	plt.savefig(title + ".png")
+	plt.close()
+
+
 if __name__ == '__main__':
-	fashion_MM_plot_from_pkl()
-	# plot_from_pkl()
-	MM_mu_1_zoom_plot_from_pkl()
-	MM_mu_01_zoom_plot_from_pkl()
-	MM_mu_05_07_08_zoom_plot_from_pkl()
-	MM_mu_05_07_08_zoom_plot_from_pkl_5_modals()
-	MM_mu_1_zoom_plot_from_pkl_5modals()
-	MM_mu_01_zoom_plot_from_pkl_5modals()
-	MM_mu_1_zoom_plot_from_pkl3modals()
-	MM_mu_01_zoom_plot_from_pkl3modals()
-	MM_mu_05_07_08_zoom_plot_from_pkl3modals()
+	# fashion_MM_plot_from_pkl()
+	# # plot_from_pkl()
+	# MM_mu_1_zoom_plot_from_pkl()
+	# MM_mu_01_zoom_plot_from_pkl()
+	# MM_mu_05_07_08_zoom_plot_from_pkl()
+	# MM_mu_05_07_08_zoom_plot_from_pkl_5_modals()
+	# MM_mu_1_zoom_plot_from_pkl_5modals()
+	# MM_mu_01_zoom_plot_from_pkl_5modals()
+	# MM_mu_1_zoom_plot_from_pkl3modals()
+	# MM_mu_01_zoom_plot_from_pkl3modals()
+	# MM_mu_05_07_08_zoom_plot_from_pkl3modals()
+	MMgeneral_plot_from_pkl("ndist_10")
