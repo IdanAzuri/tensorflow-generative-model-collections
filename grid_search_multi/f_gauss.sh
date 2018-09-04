e2c7261aa53669c066737882a08119cd26cbe591
idanazuri <idan.azuri@gmail.com>
2018-09-04 17:20:40 +0300

    sbatch

--- grid_search_multi/f_gauss.sh
+++ grid_search_multi/f_gauss.sh
@@ -5,7 +5,7 @@
 #SBATCH --time=0-20
 #SBATCH --mail-user=idan.azuri@mail.huji.ac.il
 #SBATCH --mail-type=END,FAIL,TIME_LIMIT
-#SBATCH --array=0-8%2
+#SBATCH --array=0-8%8
 SEEDS=(88 125 12 7 49 21 23 45 11)
 SEED=${SEEDS[SLURM_ARRAY_TASK_ID]}
 module load tensorflow/1.5.0
@@ -13,7 +13,7 @@ module load tensorflow/1.5.0
 dir=/cs/labs/daphna/idan.azuri/tensorflow-generative-model-collections
 
 cd $dir
-source /cs/labs/daphna/idan.azuri/venv_64/bin/activate
+source /cs/labs/daphna/idan.azuri/venv/bin/activate
 
 
 
