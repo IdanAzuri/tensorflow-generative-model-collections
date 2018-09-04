9ec0b9cdd4ace679edcd10a96b287250e0f657d1
idanazuri <idan.azuri@gmail.com>
2018-09-04 15:47:49 +0300

    sbatch

--- grid_search_multi/classifiers_4.sh
+++ grid_search_multi/classifiers_4.sh
@@ -8,7 +8,7 @@
 #SBATCH --array=0-9%2
 
 SEEDS=(88 125 12 7 49 21 23 45 11)
-SEED=${SEEDS[((SLURM_ARRAY_TASK_ID ))]}
+SEED=${SEEDS[SLURM_ARRAY_TASK_ID]}
 module load tensorflow/1.5.0
 dir=/cs/labs/daphna/idan.azuri/tensorflow-generative-model-collections
 cd $dir
---
+++ grid_search_multi/classifiers_test.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#SBATCH --mem=4g
+#SBATCH -c 4
+#SBATCH --gres=gpu:1
+#SBATCH --time=1-20
+# SBATCH --mail-user=idan.azuri@mail.huji.ac.il
+#SBATCH --mail-type=END,FAIL,TIME_LIMIT
+#SBATCH --array=0-9%2
+SEEDS=(88 125 12 7 49 21 23 45 11)
+SEED=${SEEDS[((SLURM_ARRAY_TASK_ID ))]}
+echo $SEED
+echo
+SEED=${SEEDS[SLURM_ARRAY_TASK_ID]}
+echo $SEED
+module load tensorflow/1.5.0
+
+dir=/cs/labs/daphna/idan.azuri/tensorflow-generative-model-collections
+
+cd $dir
+source /cs/labs/daphna/idan.azuri/venv_64/bin/activate
+
+
