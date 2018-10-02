50e8dd6bba148451a6089fb74d377cb30a91e79c
idanazuri <idan.azuri@gmail.com>
2018-10-02 13:16:44 +0300

    debuging

--- InfoGAN_learn_class_by_interpolation.py
+++ InfoGAN_learn_class_by_interpolation.py
@@ -112,7 +112,8 @@ class MultiModalInfoGAN_phase2(object):
 			'''
 			# self.data_y = np.delete(self.data_y, self.data_y.shape[1] - 1, axis=1)
 			# self.data_y =  np.tile(self.data_y_only9, (100, 1))
-			self.data_X = np.repeat(self.data_X_only9[None],100,axis=0).reshape(-1,28,28)
+			self.data_X = np.repeat(self.data_X_only9[None], self.n * 100, axis=0).reshape(-1, 28, 28)
+			print("self.data_X shpae {}".format(self.data_X.shape))
 			# get number of batches for a single epoch
 			self.num_batches = len(self.data_X) // self.batch_size
 		self.model_dir = self.get_model_dir()
@@ -308,7 +309,7 @@ class MultiModalInfoGAN_phase2(object):
 		for epoch in range(start_epoch, self.epoch):
 			# get batch data
 			for idx in range(start_batch_id, max(self.num_batches, 1)):
-				batch_images = self.data_X[idx * self.batch_size:(idx + 1) * self.batch_size]
+				batch_images = self.data_X[idx * self.batch_size:(idx + 1) * self.batch_size].reshape(-1, 28, 28)
 				
 				# batch_labels = np.random.multinomial(1, self.len_discrete_code * [float(1.0 / self.len_discrete_code)], size=[self.batch_size])
 				# batch_labels = simplex(dimension=self.len_discrete_code, number=self.batch_size)
