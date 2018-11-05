477be1ac9cd36ebd7a9dcb38ee8a53bc211f4e4b
idanazuri <idan.azuri@github.com>
2018-11-05 10:42:28 +0200

    retraing generator

--- InfoGAN_learn_class_by_interpolation.py
+++ InfoGAN_learn_class_by_interpolation.py
@@ -169,13 +169,13 @@ class MultiModalInfoGAN_phase2(object):
 			# y = tf.nn.softmax(y)
 			z = concat([z, y], 1)
 
-			self.layer1= lrelu(bn(linear(z, 1024, scope='g_fc1'), is_training=is_training, scope='g_bn1'))
-			self.layer2 = lrelu(bn(linear(self.layer1, 128 * self.input_height // 4 * self.input_width // 4, scope='g_fc2'), is_training=is_training, scope='g_bn2'))
+			self.layer1= lrelu(bn(linear(z, 1024, scope='retrain_g_fc1'), is_training=is_training, scope='retrain_g_bn1'))
+			self.layer2 = lrelu(bn(linear(self.layer1, 128 * self.input_height // 4 * self.input_width // 4, scope='retrain_g_fc2'), is_training=is_training, scope='retrain_g_bn2'))
 			self.layer3 = tf.reshape(self.layer2, [self.batch_size, int(self.input_height // 4), int(self.input_width // 4), 128])
 			self.layer4 = lrelu(
-				bn(deconv2d(self.layer3, [self.batch_size, int(self.input_height // 2), int(self.input_width // 2), 64], 4, 4, 2, 2, name='g_dc3'), is_training=is_training, scope='g_bn3'))
+				bn(deconv2d(self.layer3, [self.batch_size, int(self.input_height // 2), int(self.input_width // 2), 64], 4, 4, 2, 2, name='retrain_g_dc3'), is_training=is_training, scope='retrain_g_bn3'))
 
-			out = tf.nn.sigmoid(deconv2d(self.layer4, [self.batch_size, self.input_height, self.input_width, self.c_dim], 4, 4, 2, 2, name='g_dc4'))
+			out = tf.nn.sigmoid(deconv2d(self.layer4, [self.batch_size, self.input_height, self.input_width, self.c_dim], 4, 4, 2, 2, name='retrain_g_dc4'))
 			# out = tf.reshape(out, ztf.stack([self.batch_size, 784]))
 
 			return out
--- grid_search_1_shot/f_multi.sh
+++ grid_search_1_shot/f_multi.sh
@@ -18,17 +18,17 @@ source /cs/labs/daphna/idan.azuri/venv_64/bin/activate
 
 
 python3 main.py --gan_type MultiModalInfoGAN --epoch 40 --dataset mnist --sampler multi-gaussian --batch_size 64 --mu 0.7 --sigma 0.3  --ndist 5 --seed 12 --pref one-shot_gram
-python3 main.py --gan_type MultiModalInfoGAN_phase2 --epoch 6000 --dataset mnist --sampler multi-gaussian --batch_size 64 --mu 0.7 --sigma 0.3  --ndist 5 --seed 12 --pref one-shot_gram 
+python3 main.py --gan_type MultiModalInfoGAN_phase2 --epoch 6000 --dataset mnist --sampler multi-gaussian --batch_size 64 --mu 0.7 --sigma 0.3  --ndist 5 --seed 12 --pref one-shot_gram
 python3 classifier.py --dir_name /cs/labs/daphna/idan.azuri/tensorflow-generative-model-collections/ --fname mnist_MultivariateGaussianSampler_mu_0.7_sigma_0.3_ndist_5 --original mnist --seed 12
 
 python3 main.py --gan_type MultiModalInfoGAN --epoch 40 --dataset mnist --sampler multi-gaussian --batch_size 64 --mu 0.7 --sigma 0.3  --ndist 3 --seed 12 --pref one-shot_gram
-python3 main.py --gan_type MultiModalInfoGAN_phase2 --epoch 6000 --dataset mnist --sampler multi-gaussian --batch_size 64 --mu 0.7 --sigma 0.3  --ndist 3 --seed 12 --pref one-shot_gram 
+python3 main.py --gan_type MultiModalInfoGAN_phase2 --epoch 6000 --dataset mnist --sampler multi-gaussian --batch_size 64 --mu 0.7 --sigma 0.3  --ndist 3 --seed 12 --pref one-shot_gram
 python3 classifier.py --dir_name /cs/labs/daphna/idan.azuri/tensorflow-generative-model-collections/ --fname mnist_MultivariateGaussianSampler_mu_0.7_sigma_0.3_ndist_3 --original mnist --seed 12
 
 python3 main.py --gan_type MultiModalInfoGAN --epoch 40 --dataset mnist --sampler multi-gaussian --batch_size 64 --mu 0.2 --sigma 0.2  --ndist 5 --seed 12 --pref one-shot_gram
-python3 main.py --gan_type MultiModalInfoGAN_phase2 --epoch 6000 --dataset mnist --sampler multi-gaussian --batch_size 64 --mu 0.2 --sigma 0.2  --ndist 5 --seed 12 --pref one-shot_gram 
+python3 main.py --gan_type MultiModalInfoGAN_phase2 --epoch 6000 --dataset mnist --sampler multi-gaussian --batch_size 64 --mu 0.2 --sigma 0.2  --ndist 5 --seed 12 --pref one-shot_gram
 python3 classifier.py --dir_name /cs/labs/daphna/idan.azuri/tensorflow-generative-model-collections/ --fname mnist_MultivariateGaussianSampler_mu_0.2_sigma_0.2_ndist_5 --original mnist --seed 12
 
 python3 main.py --gan_type MultiModalInfoGAN --epoch 40 --dataset mnist --sampler multi-gaussian --batch_size 64 --mu 0.2 --sigma 0.2  --ndist 3 --seed 12 --pref one-shot_gram
-python3 main.py --gan_type MultiModalInfoGAN_phase2 --epoch 6000 --dataset mnist --sampler multi-gaussian --batch_size 64 --mu 0.2 --sigma 0.2  --ndist 3 --seed 12 --pref one-shot_gram 
+python3 main.py --gan_type MultiModalInfoGAN_phase2 --epoch 6000 --dataset mnist --sampler multi-gaussian --batch_size 64 --mu 0.2 --sigma 0.2  --ndist 3 --seed 12 --pref one-shot_gram
 python3 classifier.py --dir_name /cs/labs/daphna/idan.azuri/tensorflow-generative-model-collections/ --fname mnist_MultivariateGaussianSampler_mu_0.2_sigma_0.2_ndist_3 --original mnist --seed 12
